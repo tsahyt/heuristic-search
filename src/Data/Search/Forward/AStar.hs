72a2c278af9ad0d356ae78f08d9169362a211540
Paul Ogris <paul@tsahyt.com>
2017-05-04 10:03:59 +0200

    First working draft of astar

--- src/Data/Search/Forward/AStar.hs
+++ src/Data/Search/Forward/AStar.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PatternGuards #-}
 module Data.Search.Forward.AStar where
 
+import Data.Foldable
 import Data.Hashable
 import Data.HashMap.Strict (HashMap)
 import Data.HashSet (HashSet)
@@ -11,24 +13,40 @@ import qualified Data.HashMap.Lazy as M
 import qualified Data.HashSet as S
 import qualified Data.HashPSQ as Q
 
-astar :: forall a c. (Hashable a, Ord a, Ord c, Num c) 
-      => (a -> HashMap a c)         -- ^ Neighbor function
+astar :: forall a c t. (Foldable t, Eq a, Hashable a, Ord a, Ord c, Num c) 
+      => (a -> t (a,c))             -- ^ Neighbor function
       -> (a -> c)                   -- ^ Heuristic function
       -> (a -> Bool)                -- ^ Goal check
       -> a                          -- ^ Starting node
       -> Maybe [a]
 astar neighbor heuristic goal root = 
-    let h0 = heuristic root in go (Q.singleton root h0 h0) M.empty
+    let h0 = heuristic root 
+     in go (Q.singleton root h0 h0) M.empty
 
-    where go :: HashPSQ a c c -> HashMap a ([a], c) -> Maybe [a]
+    where go :: HashPSQ a c c -> HashMap a (a, c) -> Maybe [a]
           go (Q.minView -> Nothing) _ = Nothing
-          go (Q.minView -> Just (x, f, g, q')) past
-              | goal x    = reverse . fst <$> M.lookup x past
+          go (Q.minView -> Just (x, f, g, q)) past
+              | goal x    = Just . reverse $ reconstruct past x
               | otherwise = 
                   let xs  = neighbor x
-                      q'' = M.foldlWithKey' (insertQ g) q' xs
-                      p'  = _undefined
-                   in go q'' p'
+                      q'  = foldl' (\q (y,c) -> 
+                                snd $ Q.alter (updateQ y (g + c)) y q) 
+                                    q xs
+                      p'  = foldl' (\m (y,c) -> 
+                                M.alter (updateM x (g + c)) y m) 
+                                    past xs
+                   in go q' p'
           
-          insertQ g q y c = 
-              let g' = g + c in Q.insert y (g' + heuristic y) g' q
+          updateQ :: a -> c -> Maybe (c,c) -> ((), Maybe (c,c))
+          updateQ y g Nothing = ((), Just (g, g + heuristic y))
+          updateQ y g (Just (f',g')) =
+              let f = g + heuristic y
+               in if f < f' then ((), Just (f,g)) else ((), Just (f',g'))
+
+          updateM x c Nothing  = Just (x, c)
+          updateM x c (Just _) = Just (x, c)
+          
+          reconstruct past x
+              | x == root = [x]
+              | Just (x',_) <- M.lookup x past = x : reconstruct past x'
+              | otherwise = []
