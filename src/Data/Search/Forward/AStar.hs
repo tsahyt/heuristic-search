0682cb520d0983c99792a25a992b9daa72c35a75
Paul Ogris <paul@tsahyt.com>
2017-05-04 11:39:46 +0200

    Implement edge labels for astar
    This also wraps queue and map changes into one fold, and provides astar'
    for the old functionality

--- src/Data/Search/Forward/AStar.hs
+++ src/Data/Search/Forward/AStar.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE PatternGuards #-}
 module Data.Search.Forward.AStar where
 
+import Data.Bifunctor
 import Data.Foldable
 import Data.Hashable
 import Data.HashMap.Strict (HashMap)
@@ -11,44 +12,55 @@ import Data.HashPSQ (HashPSQ)
 import qualified Data.HashMap.Lazy as M
 import qualified Data.HashPSQ as Q
 
-astar :: forall a c t. (Foldable t, Eq a, Hashable a, Ord a, Ord c, Num c)
-      => (a -> t (a,c))             -- ^ Neighbor function
+astar :: forall a b c t. (Foldable t, Eq a, Hashable a, Ord a, Ord c, Num c)
+      => (a -> t (a, b, c))         -- ^ Neighbor function
       -> (a -> c)                   -- ^ Heuristic function
       -> (a -> Bool)                -- ^ Goal check
       -> a                          -- ^ Starting node
-      -> Maybe [a]
+      -> Maybe [b]
 astar neighbor heuristic goal root = 
     let h0 = heuristic root 
      in go (Q.singleton root h0 0) M.empty
 
-    where go :: HashPSQ a c c -> HashMap a (a, c) -> Maybe [a]
+    where go :: HashPSQ a c c -> HashMap a (a, b, c) -> Maybe [b]
           go (Q.minView -> Nothing) _ = Nothing
           go (Q.minView -> Just (x, _, g, q)) past
               | goal x    = Just . reverse $ reconstruct past x
               | otherwise = 
-                  let xs  = neighbor x
-                      q'  = foldl' (\z (y,c) -> 
-                                alter' (updateQ (g + heuristic y) (g + c)) y z) 
-                                    q xs
-                      p'  = foldl' (\m (y,c) -> 
-                                M.alter (updateM x (g + c)) y m) 
-                                    past xs
+                  let xs = neighbor x
+                      (q', p') = foldl' 
+                          (\z (y, l, c) -> bimap 
+                              (alter' (updateQ (g + heuristic y) (g + c)) y) 
+                              (M.alter (updateM x l (g + c)) y) z) 
+                          (q, past) xs
                    in go q' p'
           go _ _ = error "impossible"
           
           reconstruct past x
-              | x == root = [x]
-              | Just (x',_) <- M.lookup x past = x : reconstruct past x'
+              | x == root = []
+              | Just (x',l,_) <- M.lookup x past = l : reconstruct past x'
               | otherwise = []
+
+-- | Like 'astar' but without labelled edges.
+astar' :: (Functor t, Num c, Ord c, Ord a, Hashable a, Foldable t) 
+       => (a -> t (a, c))           -- ^ Neighbor function
+       -> (a -> c)                  -- ^ Heuristic function
+       -> (a -> Bool)               -- ^ Goal check
+       -> a                         -- ^ Starting node
+       -> Maybe [a]
+astar' neighbor heuristic goal root =
+    let neighbor' = fmap (fmap (\(a,c) -> (a,a,c))) neighbor
+     in fmap (root :) $ astar neighbor' heuristic goal root
           
 updateQ :: Ord c => c -> c -> Maybe (c,c) -> (Maybe (c,c))
 updateQ f g Nothing = Just (f, g)
 updateQ f g (Just (f',g')) = if f < f' then Just (f,g) else Just (f',g')
 {-# INLINE updateQ #-}
 
-updateM :: Ord c => a -> c -> Maybe (a, c) -> Maybe (a, c)
-updateM x c Nothing  = Just (x, c)
-updateM x c (Just (x',c')) = if c < c' then Just (x, c) else Just (x', c')
+updateM :: Ord c => a -> b -> c -> Maybe (a, b, c) -> Maybe (a, b, c)
+updateM x l c Nothing  = Just (x, l, c)
+updateM x l c (Just (x', l', c')) = 
+    if c < c' then Just (x, l, c) else Just (x', l', c')
 {-# INLINE updateM #-}
 
 -- | Like 'Data.HashPSQ.alter' but without the optional return parameter.
