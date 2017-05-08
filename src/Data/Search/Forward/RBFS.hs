9650e53d9e75a8fae5d544a3e6021432050c40f2
Paul Ogris <paul@tsahyt.com>
2017-05-08 14:47:54 +0200

    Add RBFS with edge labels.

--- src/Data/Search/Forward/RBFS.hs
+++ src/Data/Search/Forward/RBFS.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE ViewPatterns #-}
 module Data.Search.Forward.RBFS
 (
-    rbfs
+    rbfs,
+    rbfs'
 )
 where
 
@@ -19,40 +20,50 @@ instance Ord a => Ord (Inf a) where
     _     <= Inf   = True
     Fin a <= Fin b = a <= b
 
-rbfs :: forall a c t. (Foldable t, Hashable a, Ord a, Ord c, Num c)
-     => (a -> t (a, c))            -- ^ Neighbor function
+rbfs :: forall a b c t. (Foldable t, Hashable a, Ord a, Ord c, Num c)
+     => (a -> t (a, b, c))         -- ^ Neighbor function
      -> (a -> c)                   -- ^ Heuristic function
      -> (a -> Bool)                -- ^ Goal check
      -> a                          -- ^ Starting node
-     -> Maybe [a]
+     -> Maybe [b]
 rbfs neighbor heuristic goal root = 
     case go root Inf (Fin $ heuristic root, 0) of
         Left  _ -> Nothing
         Right x -> Just x
 
-    where go :: a -> Inf c -> (Inf c, c) -> Either (Inf c) [a]
+    where go :: a -> Inf c -> (Inf c, c) -> Either (Inf c) [b]
           go x fLimit (f, g)
-              | goal x = Right [x]
+              | goal x = Right []
               | otherwise =
                   let xs = Q.fromList . map (buildS f g)
                          . toList . neighbor $ x
-                   in fmap (x :) $ loop fLimit xs
+                   in loop fLimit xs
           
-          buildS f g (a, c) =
+          buildS f g (a, b, c) =
               let g' = g + c
                   f' = max (Fin $ g' + heuristic a) f
-               in (a, f', g')
+               in (a, f', (b, g'))
 
           loop _ (Q.minView -> Nothing) = Left Inf
-          loop fLimit (Q.minView -> Just (x, f, g, q))
+          loop fLimit (Q.minView -> Just (x, f, (b, g), q))
               | f > fLimit = Left f
               | otherwise =
                   let alt = fromMaybe Inf . fmap mid . Q.findMin $ q
                    in case go x (min fLimit alt) (f, g) of
-                          Left f' -> loop fLimit (Q.insert x f' g q)
-                          Right z -> Right z
+                          Left f' -> loop fLimit (Q.insert x f' (b, g) q)
+                          Right z -> Right $ b : z
 
           loop _ _ = error "impossible"
+
+rbfs' :: forall a c t. (Functor t, Foldable t, Hashable a, Ord a, Ord c, Num c)
+      => (a -> t (a, c))            -- ^ Neighbor function
+      -> (a -> c)                   -- ^ Heuristic function
+      -> (a -> Bool)                -- ^ Goal check
+      -> a                          -- ^ Starting node
+      -> Maybe [a]
+rbfs' neighbor heuristic goal root =
+    let neighbor' = fmap (fmap (\(a,c) -> (a,a,c))) neighbor
+     in fmap (root :) $ rbfs neighbor' heuristic goal root
 
 mid :: (a, b, c) -> b
 mid (_,x,_) = x
