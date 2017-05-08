046e5448b4ce150f5952a4a7d3dd4c7170e349e5
Paul Ogris <paul@tsahyt.com>
2017-05-08 17:15:29 +0200

    Add breadth first traversals

--- src/Data/Search/Forward/NonOptimal.hs
+++ src/Data/Search/Forward/NonOptimal.hs
@@ -1,14 +1,20 @@
 module Data.Search.Forward.NonOptimal
 (
+    -- * Depth First
     dft,
     dftT,
     dfs,
-    dfsT
+    dfsT,
+
+    -- * Breadth First
+    bft,
+    bftT
 )
 where
 
 import Data.Foldable
 import Data.Hashable
+import Data.Sequence (Seq)
 
 import qualified Data.HashSet as HS
 
@@ -50,7 +56,9 @@ dfs suc goal = go HS.empty
                     let v' = n `HS.insert` visited
                      in (n :) <$> asum [ go v' x | x <- toList (suc n) ]
 
-dfsT :: (Functor t, Foldable t)
+-- | Like 'dfs' but traversing a tree. This has the potential to loop
+-- indefinitely in graphs with cycles!
+dfsT :: Foldable t
      => (a -> t a)          -- ^ Successor function
      -> (a -> Bool)         -- ^ Goal check
      -> a                   -- ^ Starting node
@@ -59,3 +67,26 @@ dfsT suc goal = go
     where go n 
               | goal n    = Just [n]
               | otherwise = (n :) <$> asum [ go x | x <- toList (suc n) ]
+
+-- | 'bft' performs a breadth first traversal of a graph with unlabeled edges.
+-- Nodes are returned in the order they are visited.
+bft :: (Foldable t, Hashable a, Eq a)
+    => (a -> t a)
+    -> t a
+    -> [a]
+bft suc = go HS.empty . toList
+    where go _ [] = []
+          go visited (n:ns) 
+              | n `HS.member` visited = go visited ns
+              | otherwise = n : go (n `HS.insert` visited) 
+                                   (ns ++ toList (suc n))
+
+-- | Like 'bft' but traversing a tree. This has the potential to loop
+-- indefinitely in graphs with cycles!
+bftT :: Foldable t
+     => (a -> t a)
+     -> t a
+     -> [a]
+bftT suc = go . toList
+    where go [] = []
+          go (n:ns) = n : go (ns ++ toList (suc n))
