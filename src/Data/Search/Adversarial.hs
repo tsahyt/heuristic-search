349f49c0a8dd4e85f348b9a64db2634de3d0d770
Paul Ogris <paul@tsahyt.com>
2017-05-30 19:18:25 +0200

    Add minimax

--- src/Data/Search/Adversarial.hs
+++ src/Data/Search/Adversarial.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Data.Search.Adversarial
 (
-    negmax
+    negmax,
+    minimax
 )
 where
 
@@ -32,3 +33,33 @@ negmax cutoff maxSucc minSucc eval term root =
                      in if null xs then eval x 
                         else maximum 
                       . fmap (negate . go (pred d) (not p) . fst) $ xs
+
+minimax :: forall a b c t. (Foldable t, Functor t, Ord c, Num c)
+        => Natural                   -- ^ Depth limit
+        -> (a -> t (a, b))           -- ^ Max player moves
+        -> (a -> t (a, b))           -- ^ Min player moves
+        -> (a -> c)                  -- ^ Evaluation function
+        -> (a -> Bool)               -- ^ Terminal check
+        -> a                         -- ^ Root node
+        -> Maybe b
+minimax cutoff maxSucc minSucc eval term root =
+    let xs = fmap (first (gomin (fromIntegral cutoff))) (maxSucc root)
+     in if null xs 
+        then Nothing 
+        else Just . snd . maximumBy (comparing fst) $ xs
+    where gomax, gomin :: Int -> a -> c
+          gomax 0 x = eval x
+          gomax d x
+              | term x = eval x
+              | otherwise =
+                    let xs = maxSucc x 
+                     in if null xs then eval x
+                        else maximum . fmap (gomin (pred d) . fst) $ xs
+
+          gomin 0 x = eval x
+          gomin d x
+              | term x = eval x
+              | otherwise =
+                    let xs = minSucc x
+                     in if null xs then eval x
+                        else minimum . fmap (gomax (pred d) . fst) $ xs
